9518fa18c55e33ed690d4c387e648dd310627ac1
Mario_Segovia <mario220696@gmail.com>
2018-05-22 16:18:33 +0000

    Agrego vistas para la creación y edición de blogs

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
 	path('', views.index, name='index'),
 	path('<int:pk>', views.post_detail, name='post_detail'),
+	path(r'^post/new/', views.post_new, name='post_new'),
 ]
